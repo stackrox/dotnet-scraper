e57b93cd8d0be5930a292aba890f2c7f47522f8e
Connor Gorman <cgorman@stackrox.com>
2020-10-20 12:51:15 -0700

    more cleanup

--- croncheck/main.go
+++ croncheck/main.go
@@ -9,8 +9,9 @@ import (
 	"strings"
 
 	"github.com/ghodss/yaml"
-	"github.com/google/go-github/v32/github"
 	"github.com/stackrox/dotnet-scraper/types"
+
+	"github.com/google/go-github/v32/github"
 )
 
 type repoReference struct {
@@ -29,23 +30,40 @@ var repos = []repoReference{
 }
 
 var knownMissingLinks = []string{
-	"https://github.com/aspnet/Announcements/issues/405",
+	// Microsoft Security Advisory: iOS12 breaks social, WSFed and OIDC logins #318
 	"https://github.com/aspnet/Announcements/issues/318",
+
+	// Microsoft Security Advisory ASPNETCore-July18: ASP.NET Core Denial Of Service Vulnerability
 	"https://github.com/aspnet/Announcements/issues/311",
+
+	// Microsoft Security Advisory ASPNETCore-Mar18: ASP.NET Core Denial Of Service Vulnerability
 	"https://github.com/aspnet/Announcements/issues/300",
+
+	// Microsoft Security Advisory CVE-2020-1597 | ASP.NET Core Denial of Service Vulnerability
+	// Duplicate
 	"https://github.com/dotnet/announcements/issues/162",
-	"https://github.com/dotnet/announcements/issues/51",
+
+	// Microsoft Security Advisory CVE-2019-0815: ASP.NET Core denial of service vulnerability
+	// Strange module
 	"https://github.com/aspnet/Announcements/issues/352",
+
+	// Microsoft Security Advisory CVE-2020-1108 | .NET Core Denial of Service Vulnerability
+	// Duplicate of "https://github.com/dotnet/announcements/issues/157"
 	"https://github.com/dotnet/announcements/issues/156",
+
+	// Microsoft Security Advisory 4021279: Vulnerabilities in .NET Core, ASP.NET Core Could Allow Elevation of Privilege
+	// Duplicate of https://github.com/dotnet/announcements/issues/12
 	"https://github.com/aspnet/Announcements/issues/239",
+
+	// Microsoft Security Advisory CVE-2018-0808: ASP.NET Core Denial Of Service Vulnerability
+	// IIS vuln
 	"https://github.com/aspnet/Announcements/issues/294",
-	"https://github.com/dotnet/announcements/issues/12",
 }
 
 func main() {
 	client := github.NewClient(nil)
 
-	issueLinks := make(map[string]struct{})
+	issueLinks := make(map[string]bool)
 	for _, repo := range repos {
 		issues, _, err := client.Issues.ListByRepo(context.Background(), repo.owner, repo.name, &github.IssueListByRepoOptions{
 			Labels: []string{"security"},
@@ -63,39 +81,51 @@ func main() {
 				continue
 			}
 			link := strings.ReplaceAll(issue.GetURL(), "api.github.com/repos", "github.com")
-			issueLinks[link] = struct{}{}
+			issueLinks[link] = true
 		}
+	}
 
-		root := "cves"
-		err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	root := "cves"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Ext(info.Name()) == ".yaml" {
+			bytes, err := ioutil.ReadFile(filepath.Join(root, info.Name()))
 			if err != nil {
 				return err
 			}
-			if filepath.Ext(info.Name()) == ".yaml" {
-				bytes, err := ioutil.ReadFile(filepath.Join(root, info.Name()))
-				if err != nil {
-					return err
-				}
-				ff := &types.FileFormat{}
-				if err := yaml.Unmarshal(bytes, ff); err != nil {
-					log.Fatalf("could not parse yaml file %s: %v", info.Name(), err)
-				}
-				delete(issueLinks, ff.Link)
+			ff := &types.FileFormat{}
+			if err := yaml.Unmarshal(bytes, ff); err != nil {
+				log.Fatalf("could not parse yaml file %s: %v", info.Name(), err)
 			}
-			return nil
-		})
-		if err != nil {
-			log.Fatalf("error walking cve dir: %v", err)
+			_, ok := issueLinks[ff.Link]
+			if !ok {
+				log.Fatalf("unknown link %v - %v", ff.Link, info.Name())
+			} else {
+				issueLinks[ff.Link] = false
+			}
 		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("error walking cve dir: %v", err)
 	}
+
 	// Remove all known missing links. Why the links are missing should be next to their declaration
 	for _, knownMissing := range knownMissingLinks {
 		delete(issueLinks, knownMissing)
 	}
 	if len(issueLinks) > 0 {
-		for link := range issueLinks {
-			log.Printf("Unaccounted for issue: %v", link)
+		fail := false
+		for link, val := range issueLinks {
+			if val {
+				fail = true
+				log.Printf("Unaccounted for issue: %v", link)
+			}
 		}
-		log.Fatalf("Failing with non 0 exit code")
+		if fail {
+			log.Fatalf("Failing with non 0 exit code")
+		}
 	}
 }
